a8d59940f7d55e9f86bbb287b18447670a73e544
d8a0 <151666577+d8a0@users.noreply.github.com>
2023-12-07 11:08:33 +0000

    Co-authored-by: Alfie <alfredonline@users.noreply.github.com>

--- quiz.js
+++ quiz.js
@@ -1,5 +1,5 @@
 const question = document.querySelector('#question');
-const choices = Array.from(document.querySelectorAll('.choice-text'));
+const choice = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
@@ -12,44 +12,44 @@ let availableQuestions = []
 
 let questions = [
     {
-        question: 'What is the capital of Equador?'
-        choice1: '',
-        choice2: 'x',
-        choice3: 'x',
-        choice4: 'x',
-        answer: 1,
-    }
+        question: 'What is the capital of Equador?',
+        choiceOne: 'Nuuk',
+        choiceTwo: 'Quito',
+        choiceThree: 'Suva',
+        choiceFour: 'San Salvador',
+        answer: 'Quito'
+    },
     {
-        question: 'what is ...'
-        choice1: 'x',
-        choice2: 'x',
-        choice3: 'x',
-        choice4: 'x',
-        answer: 1,
-    }
+        question: 'what is ...',
+        choiceOne: 'x',
+        choiceTwo: 'x',
+        choiceThree: 'x',
+        choiceFour: 'x',
+        answer: ''
+    },
     {
-        question: 'what is ...'
-        choice1: 'x',
-        choice2: 'x',
-        choice3: 'x',
-        choice4: 'x',
-        answer: 1,
-    }
+        question: 'what is ...',
+        choiceOne: 'x',
+        choiceTwo: 'x',
+        choiceThree: 'x',
+        choiceFour: 'x',
+        answer: ''
+    },
     {
-        question: 'what is ...'
-        choice1: 'x',
-        choice2: 'x',
-        choice3: 'x',
-        choice4: 'x',
-        answer: 1,
-    }
+        question: 'what is ...',
+        choiceOne: 'x',
+        choiceTwo: 'x',
+        choiceThree: 'x',
+        choiceFour: 'x',
+        answer: ''
+    },
     {
-        question: 'what is ...'
-        choice1: 'x',
-        choice2: 'x',
-        choice3: 'x',
-        choice4: 'x',
-        answer: 1,
+        question: 'what is ...',
+        choiceOne: 'x',
+        choiceTwo: 'x',
+        choiceThree: 'x',
+        choiceFour: 'x',
+        answer: ''
     }
 ]
 
@@ -70,25 +70,27 @@ getNewQuestion = () => {
     }
     questionCounter++
     progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
-    progressBarFull.computedStyleMap.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`
 
     const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
-    currentQuestion = availableQuestions(questionsIndex)
+    currentQuestion = availableQuestions[questionsIndex]
     question.innerText = currentQuestion.question
 
-    choices.forEach(choice => { const number = choice.dataset['number'] })
-    choice.innerText = currentQuestion['choice' + number]
+    choice.forEach(choice => {
+        const number = choice.innerText['number']
+        choice.innerText = currentQuestion['word' + number]
+    })
     availableQuestions.splice(questionsIndex, 1)
 
     acceptingAnswers = true
 }
 
-choices.forEach(choice => {
+choice.forEach(choice => {
     choice.addEventListener('click', e => {
         if (!acceptingAnswers) return
         acceptingAnswers = false
         const selectedChoice = e.target
-        const selectedAnswer = selected.dataset['number']
+        const selectedAnswer = selected.innerText['number']
 
         let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
 
@@ -110,3 +112,4 @@ incrementScore = num => {
 }
 
 startGame()
+
